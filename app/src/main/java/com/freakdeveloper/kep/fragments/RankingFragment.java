806bac1fc36b68cfb58a62f923aa55ab65add096
Daniel Ochoa <dany8a09@gmail.com>
2019-05-04 17:28:11 -0500

    Ranking

--- app/src/main/java/com/freakdeveloper/kep/fragments/RankingFragment.java
+++ app/src/main/java/com/freakdeveloper/kep/fragments/RankingFragment.java
@@ -53,25 +53,30 @@ public class RankingFragment extends Fragment {
     //VARIABLES
     private ArrayList<Ranking> Datos=new ArrayList<>();
     private ArrayList<String> id=new ArrayList<>();
-    public String Nick="",Escuela="";
-    public int Lugar=0,Num=0;
+    public String Nick,Escuela;
+    public int Lugar,Num;
     float m;
     private int[] totales=new int[11];
     private int[] aciertos=new int[11];
     private Float[] Punta;
     private String[] ID;
     private ArrayList<Float> Pun = new ArrayList<>();
+    private ArrayList<Float> Pun3 = new ArrayList<>();
     private Ranking global=new Ranking();
+    private Ranking escolar=new Ranking();
     //var 2
     private ArrayList<Ranking> Datos2=new ArrayList<>();
     private ArrayList<String> id2=new ArrayList<>();
+    private ArrayList<String> id3=new ArrayList<>();
     String Nick2="",Escuela2="";
     int Lugar2=0,Num2=0;
     float m2;
     private int[] totales2=new int[11];
     private int[] aciertos2=new int[11];
     private Float[] Punta2;
+    private Float[] Punta3;
     private String[] ID2;
+    private String[] ID3;
     private ArrayList<Float> Pun2 = new ArrayList<>();
     private Ranking global2=new Ranking();
 
@@ -105,13 +110,17 @@ public class RankingFragment extends Fragment {
 
         reclyclerViewR= (RecyclerView) v.findViewById(R.id.RVidR);
         reclyclerViewR.setLayoutManager(new LinearLayoutManager(v.getContext() , LinearLayoutManager.VERTICAL , false));
-        reclyclerViewRE= (RecyclerView) v.findViewById(R.id.RVidRE);
-        reclyclerViewRE.setLayoutManager(new LinearLayoutManager(v.getContext() , LinearLayoutManager.VERTICAL , false));
 
 
 
         //Trae Registros
-
+        Nick="";
+        Nick2="";
+        Escuela="";
+        Num=0;
+        Num2=0;
+        Lugar=0;
+        Lugar2=0;
         user = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -122,12 +131,13 @@ public class RankingFragment extends Fragment {
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                         Persona persona = snapshot.getValue(Persona.class);
                         String xID=persona.getIdPersona();
-                        //carre.add(i, persona.getIdPersona());
+
                         if(xID.equals(user.getUid()))
                         {
                             Nick=persona.getNickName();
+                            Nick2=Nick;
                             Escuela=persona.getEscingresar();
-                            break;
+                               break;
                         }
 
                     }
@@ -141,6 +151,30 @@ public class RankingFragment extends Fragment {
             }
         });
         //traeNickName();
+        databaseReference.child(nodoPersona).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Persona persona = snapshot.getValue(Persona.class);
+                        String Escu=persona.getEscingresar();
+
+                        if(Escu.equals(Escuela))
+                        {
+                            id2.add(persona.getIdPersona());
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        //obtenIdEsc();
 
         databaseReference.child(nodoRespuestas).addValueEventListener(new ValueEventListener() {
             @Override
@@ -194,6 +228,12 @@ public class RankingFragment extends Fragment {
                     {
                         Punta[i]=Pun.get(i);
                         ID[i]=id.get(i);
+                        for (int j=0;j<id2.size();j++) {
+                            if (id2.get(j).equals(id.get(i))) {
+                                id3.add(id2.get(j));
+                                Pun3.add(Pun.get(i));
+                            }
+                        }
                     }
 
                     float aux;
@@ -209,154 +249,33 @@ public class RankingFragment extends Fragment {
                                 ID[x]=auxid;
                             }
                         }
-                    }
-                    //ordenarPuntaje();
 
-                    int n=0;
-                    for (int i=0;i<ID.length;i++)
+                    }
+                    Punta3=new Float[Pun3.size()];
+                    ID3=new String[id3.size()];
+                    for (int k=0;k<id3.size();k++)
                     {
-                        n=i+1;
-                        if(ID[i].equals(user.getUid()))
-                        {
-                            Lugar=n;
-                            break;
-                        }
+                        Punta3[k]=Pun3.get(k);
+                        ID3[k]=id3.get(k);
                     }
 
-                    //Lugar=obtenLugar();
-                    Num=ID.length;
-                    global.setImagen(R.drawable.mex);
-                    global.setNickName(Nick);
-                    global.setPosicion(Lugar);
-                    global.setNumUsu(Num);
 
-                    Datos.add(global);
-
-                    Ranking escolar=new Ranking(R.drawable.gorro,"Javier",12,34);
-                    Datos.add(escolar);
-
-
-                    RankingAdapterRecyclerView Adap = new RankingAdapterRecyclerView(Datos);
-                    reclyclerViewR.setAdapter(Adap);
-                    //Toast.makeText(getContext(), "nick"+Nick+"lugar"+Lugar+"num"+Num, Toast.LENGTH_SHORT).show();
-                }
-            }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        Toast.makeText(getContext(), "nick"+Nick+"lugar"+Lugar+"num"+Num, Toast.LENGTH_SHORT).show();
-        /*
-        databaseReference.child(nodoPersona).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        Persona persona = snapshot.getValue(Persona.class);
-                        String xID=persona.getIdPersona();
-                        //carre.add(i, persona.getIdPersona());
-                        if(xID.equals(user.getUid()))
-                        {
-                            Nick=persona.getNickName();
-                            Escuela=persona.getEscingresar();
-                            break;
-                        }
-
-                    }
-                    //Toast.makeText(getContext(), "nick "+Nick+" lugar "+Lugar+" numusu "+Num , Toast.LENGTH_SHORT).show();
-                    /*
-                    for(int j=0;j<i;j++)
-                    {
-                        if(carre.get(j).equals(Escuela))
-                        {
-                            y=y+1;
-                            carreras[y]=carre.get(j);
-                        }
-                    }*
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        //traeNickName();
-
-        databaseReference.child(nodoRespuestas).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
-                        Respuestas respuestas = snapshot.getValue(Respuestas.class);
-                        String IDres=snapshot.getKey();
-                        int[] y=new int[2];
-                        aciertos[0]=respuestas.getAlgebra();
-                        totales[0]=respuestas.getTotalAlgebra();
-                        aciertos[1]=respuestas.getBiologia();
-                        totales[1]=respuestas.getTotalBiologia();
-                        aciertos[2]=respuestas.getCalculoDiferencialeIntegral();
-                        totales[2]=respuestas.getTotalCalculoDiferencialeIntegral();
-                        aciertos[3]=respuestas.getComprensiondeTextos();
-                        totales[3]=respuestas.getTotalComprensiondeTextos();
-                        aciertos[4]=respuestas.getFisica();
-                        totales[4]=respuestas.getTotalFisica();
-                        aciertos[5]=respuestas.getGeometriaAnalitica();
-                        totales[5]=respuestas.getTotalGeometriaAnalitica();
-                        aciertos[6]=respuestas.getGeometriayTrigonometria();
-                        totales[6]=respuestas.getTotalGeometriayTrigonometria();
-                        aciertos[7]=respuestas.getProbabilidadyEstadistica();
-                        totales[7]=respuestas.getTotalProbabilidadyEstadistica();
-                        aciertos[8]=respuestas.getProduccionEscrita();
-                        totales[8]=respuestas.getTotalProduccionEscrita();
-                        aciertos[9]=respuestas.getQuimica();
-                        totales[9]=respuestas.getTotalQuimica();
-                        aciertos[10]=respuestas.getRazonamientoMatematico();
-                        totales[10]=respuestas.getTotalRazonamientoMatematico();
-
-                        //y=puntaje();
-
-                        for(int i=0;i<11;i++)
-                        {
-                            y[0]=y[0]+aciertos[i];
-                            y[1]=y[1]+totales[i];
-                        }
-
-                        m=(float)y[0]/(float)y[1];
-
-                        Pun.add(m);
-                        id.add(IDres);
-
-                    }
-
-                    Punta=new Float[Pun.size()];
-                    ID=new String[id.size()];
-                    for(int i=0;i<Pun.size();i++)
-                    {
-                        Punta[i]=Pun.get(i);
-                        ID[i]=id.get(i);
-                    }
-
-                    float aux;
-                    String auxid;
-                    for (int i = 0; i < Punta.length - 1; i++) {
-                        for (int x = i + 1; x < Punta.length; x++) {
-                            if (Punta[x] > Punta[i]) {
-                                aux = Punta[i];
-                                auxid= ID[i];
-                                Punta[i]=Punta[x];
-                                ID[i]=ID[x];
-                                Punta[x]=aux;
-                                ID[x]=auxid;
+                    float aux3;
+                    String auxid3;
+                    for (int i = 0; i < Pun3.size() - 1; i++) {
+                        for (int x = i + 1; x < Pun3.size(); x++) {
+                            if (Punta3[x] > Punta3[i]) {
+                                aux3 = Punta3[i];
+                                auxid3= ID3[i];
+                                Punta3[i]=Punta3[x];
+                                ID3[i]=ID3[x];
+                                Punta3[x]=aux3;
+                                ID3[x]=auxid3;
                             }
                         }
+
                     }
                     //ordenarPuntaje();
-
                     int n=0;
                     for (int i=0;i<ID.length;i++)
                     {
@@ -368,22 +287,36 @@ public class RankingFragment extends Fragment {
                         }
                     }
 
+                    int o=0;
+                    for (int i=0;i<ID3.length;i++)
+                    {
+                        o=i+1;
+                        if(ID3[i].equals(user.getUid()))
+                        {
+                            Lugar2=o;
+                            break;
+                        }
+                    }
+
                     //Lugar=obtenLugar();
                     Num=ID.length;
                     global.setImagen(R.drawable.mex);
                     global.setNickName(Nick);
                     global.setPosicion(Lugar);
                     global.setNumUsu(Num);
-
                     Datos.add(global);
 
-                    Ranking escolar=new Ranking(R.drawable.gorro,"Javier",12,34);
+                    Num2=ID3.length;
+                    escolar.setImagen(R.drawable.gorro);
+                    escolar.setNickName(Nick2);
+                    escolar.setPosicion(Lugar2);
+                    escolar.setNumUsu(Num2);
                     Datos.add(escolar);
 
 
                     RankingAdapterRecyclerView Adap = new RankingAdapterRecyclerView(Datos);
                     reclyclerViewR.setAdapter(Adap);
-                    //Toast.makeText(getContext(), "nick"+Nick+"lugar"+Lugar+"num"+Num, Toast.LENGTH_SHORT).show();
+
                 }
             }
 
@@ -393,10 +326,10 @@ public class RankingFragment extends Fragment {
 
             }
         });
-        */
 
         return v;
     }
+
 
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -428,4 +361,6 @@ public class RankingFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
